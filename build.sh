80211eff1e7e5b2aa28d6c3598d36dd9be22d582
Shion Tanaka <shtanaka@redhat.com>
2021-08-25 09:02:55 +0900

    update build.sh

--- build.sh
+++ build.sh
@@ -5,26 +5,26 @@ CURDIR=$PWD
 cd Containerfile.d
 
 # Building Image
-
 for f in Containerfile*
 do
-    echo "### Build ${f/Containerfile./} ###"
+    echo -e "\n### Build ${f/Containerfile./} ###"
     buildah bud -t powercli:${f/Containerfile./} -f $f
 done
 
 # Test
 for f in Containerfile*
 do
-    echo "### Run ${f/Containerfile./} ###"
+    echo -e "\n### Run ${f/Containerfile./} ###"
     podman run --rm powercli:${f/Containerfile./} pwsh -v
 done
 
 # Push to Docker.io
 for f in Containerfile*
 do
-    echo "### Push ${f/Containerfile./} ###"
+    echo -e "\n### Push powercli:${f/Containerfile./} ###"
     podman push powercli:${f/Containerfile./} docker.io/tnk4on/powercli:${f/Containerfile./} --format v2s2
     if [ "${f/Containerfile./}" = "mcr-alpine-3.12" ]; then
+        echo -e "\n### Push powercli:latest"
         podman push powercli:${f/Containerfile./} docker.io/tnk4on/powercli --format v2s2
     fi
 done
